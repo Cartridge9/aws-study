a38b3c9ee9c4febc8ec1917bc796b025da9029f5
Cartridge0923 <pjmin0923@gmail.com>
2026-06-17 09:23:58 +0900

    first commit

---
+++ src/main/java/dev/pjmin/cicd_springboot_01/CicdSpringboot01Application.java
@@ -0,0 +1,13 @@
+package dev.pjmin.cicd_springboot_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdSpringboot01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdSpringboot01Application.class, args);
+    }
+
+}
---
+++ src/test/java/dev/pjmin/cicd_springboot_01/CicdSpringboot01ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pjmin.cicd_springboot_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdSpringboot01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
